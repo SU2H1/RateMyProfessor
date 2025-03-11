3a24aeca2b19ffcd1f4952579270aeaf29bd6ab1
SU2H1 <ka1t0su2h1@gmail.com>
2025-03-11 19:33:45 +0900

    Added ToS to the bottom of home.php

--- home.php
+++ home.php
@@ -1130,6 +1130,12 @@ elseif (!isset($_COOKIE['language'])) {
         </div>
         
         <footer>
+        <p>&copy; 2025 Rate My Teacher. All rights reserved.</p>
+    <p style="margin-top: 10px;">
+        <a href="terms-conditions.php?lang=<?php echo $currentLang; ?>" style="color: white; text-decoration: underline;">
+            <?php echo $currentLang == 'ja' ? '利用規約' : 'Terms and Conditions'; ?>
+        </a>
+    </p>
         </footer>
     </div>
     
